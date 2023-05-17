8a36f39b66552333bcaa4c12f80da71902eed9c1
Grayson Shogan <graysontoddshogan@gmail.com>
2023-05-17 16:57:25 -0700

    wip

--- src/components/AddFriendForm.js
+++ src/components/AddFriendForm.js
@@ -1,1 +1,62 @@
-import 
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function AddFriendForm() {
+  const [creds, setCreds] = useState({
+    id: "",
+    name: "",
+    age: "",
+    email: "",
+  });
+
+  const onChange = (e) => {
+    setCreds({
+      ...creds,
+      id: Date.now,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:9000/api/friends", creds)
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div>
+      <form>
+        <label onSubmit={handleSubmit}>
+          {""}
+          friend name
+          <input
+            type="text"
+            calue={creds.name}
+            name="name"
+            onChange={onChange}
+          />
+        </label>
+        <label>
+          {" "}
+          friend age
+          <input type="text" name="age" value={creds.age} onChange={onChange} />
+        </label>
+        <label>
+          {" "}
+          friend email
+          <input
+            type="text"
+            name="email"
+            value={creds.email}
+            onChange={onChange}
+          />
+        </label>
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
